ac1f7dfa8db596623af9c1b94cbbc6d25fb6d615
taehui <hth4236727@gmail.com>
2025-12-30 21:50:27 +0900

    interface와 union타입 활용해보기
    배송 방법은 3개로 고정

--- typescript/ts-day2/src/exam.ts
+++ typescript/ts-day2/src/exam.ts
@@ -9,3 +9,26 @@ const taehui: Student = {
   age: 23,
   introduce: () => console.log("나는 태희"),
 };
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+type DeliveryMethod = "택배" | "방문수령" | "새벽배송";
+
+interface ProductDetail extends Product {
+  description: string;
+  delivery: DeliveryMethod;
+  stock: number;
+}
+
+const myProduct: ProductDetail = {
+  id: 1,
+  name: "초대형거인 피규어",
+  price: 5000,
+  description: "한정판매",
+  delivery: "새벽배송",
+  stock: 50,
+};
